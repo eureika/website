c53ad81b41e756196c96baa5f68b78a43f47caeb
eureika <xxreika@gmail.com>
2015-07-02 16:50:02 -0700

    First Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,29 @@
+class PagesController < ApplicationController
+  def index
+  end
+  def reel
+  end
+  def models
+  end
+  def designs    
+  end
+  def projects
+  end
+  def others
+  end
+  def resume
+  end
+
+  def card_series
+  end
+  def pennappetit
+  end
+  def journal_wax
+  end
+
+  def animated_short
+  end
+  def afterglow
+  end
+
+end
